945705a4a1176536692a6b1be75ff60a289c6580
TheOnlyMrLucifer <69217587+TheOnlyMrLucifer@users.noreply.github.com>
2021-02-22 14:01:59 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 module.exports = async (client) => {
   console.log(`[API] Logged in as ${client.user.username}`);
-  await client.user.setActivity("Music", {
+  await client.user.setActivity("v2.0 By The Mr Lucifer", {
     type: "LISTENING",//can be LISTENING, WATCHING, PLAYING, STREAMING
   });
 };
